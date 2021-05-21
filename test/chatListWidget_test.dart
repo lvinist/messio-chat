c088a67420caf279548be0b7d0d161c61e1be68e
Muhammad Alvin Ali Fikri <alvin.geomatics@gmail.com>
2021-05-21 14:47:33 +0700

    Update chatListWidget_test.dart

--- test/chatListWidget_test.dart
+++ test/chatListWidget_test.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:messio/widget/chatListWidget.dart';
+import 'package:messio/main.dart';
 
 
 void main() {
   testWidgets('ChatListWidget UI Test', (WidgetTester tester) async {
-    await tester.pumpWidget(app);
+    await tester.pumpWidget(Messio());
 
     expect(find.byType(ListView), findsOneWidget);
   });
