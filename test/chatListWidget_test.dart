a4da3365d8592e1edb2a49da7c2374934b12c275
Muhammad Alvin Ali Fikri <alvin.geomatics@gmail.com>
2021-05-21 14:41:34 +0700

    update chatlistwidget test

--- test/chatListWidget_test.dart
+++ test/chatListWidget_test.dart
@@ -2,12 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:messio/widget/chatListWidget.dart';
 
-void main() {
-  MaterialApp app = MaterialApp(
-    home: Scaffold(
-      body: ChatListWidget(),
-    ),
-  );
 
   testWidgets('ChatListWidget UI Test', (WidgetTester tester) async {
     await tester.pumpWidget(app);
